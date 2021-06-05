7e8c3f568bbe1dd02451cb16b1418a347ea285b3
ChristopherLiew <52255272+ChristopherLiew@users.noreply.github.com>
2021-06-06 00:32:07 +0800

    Updated generalisable transformer to Albert w/ configuration; Housekeeping

---
+++ models/sent_clf/lstm_torch.py
@@ -0,0 +1,5 @@
+import torch
+import pytorch_lightning
+import pandas as pd 
+import numpy as np
+
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,11 @@
 import os
 import time
 import pickle
+from numpy.core.records import array
 import pandas as pd
+from time import sleep
+from typing import Dict, List, Optional, Tuple
+from sklearn.base import BaseEstimator
 from ast import literal_eval
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import multilabel_confusion_matrix, classification_report
@@ -9,47 +13,61 @@ from gensim import models
 from tensorflow.keras import utils
 from tensorflow import data
 
-def get_clf_results(y_true, y_pred):
-    print(multilabel_confusion_matrix(y_true, y_pred))
-    return pd.DataFrame(classification_report(y_true, y_pred, output_dict=True))
 
-def tune_model(model, X, search_params, verbosity=True, n_jobs=-1):
+def get_clf_results(y_true: array, y_pred: array) -> pd.DataFrame:
+    print(multilabel_confusion_matrix(y_true, y_pred))
+    return pd.DataFrame(classification_report(y_true, y_pred, 
+                                              output_dict=True))
+
+
+def tune_model(model: BaseEstimator, data: pd.DataFrame,
+               search_params: Dict, verbosity: bool = True,
+               n_jobs: int = -1) -> Tuple:
+
     tuner = RandomizedSearchCV(
         model, search_params, verbose=verbosity, n_jobs=n_jobs)
-    train_data, train_labels = X
+    train_data, train_labels = data
     search = tuner.fit(train_data, train_labels)
     return search.best_estimator_, search.best_score_, search.best_params_
 
-def save_model(model, filepath):
+
+def save_model(model: BaseEstimator, filepath: str) -> None:
     with open(filepath, 'wb') as file:
         pickle.dump(model, file)
         print('Model successfully saved at: ' + filepath)
 
-def load_model(filepath):
+
+def load_model(filepath: str):
     with open(filepath, 'rb') as file:
         print('Loading model from: ' + filepath)
         return pickle.load(file)
 
-def convert_to_lists(string_data):
+
+def convert_to_lists(string_data: str) -> List:
     '''
     Use when converting back from csv data.
     '''
     return [literal_eval(i) for i in string_data]
 
-def create_bigrams(text, min_count=20):
+
+def create_bigrams(text: List[str], min_count: int = 20) -> List:
     sleep(0.1)
     # Higher min_words and threshold-> Harder to form bigrams
     bigram = models.Phrases(text, min_count)
-    bigram_mod = models.phrases.Phraser(
-        bigram)  # Creates a memory efficient model of phrases w/o model state (Ok if we do not need to train on new docs)
-    return [bigram_mod[doc] for doc in text]  # Transform doc to bigrams
+    # Creates a memory efficient model of phrases w/o model state
+    bigram_mod = models.phrases.Phraser(bigram)
+    # Transform doc to bigrams
+    return [bigram_mod[doc] for doc in text]  
 
-def create_trigrams(bigrams, min_count=5):
+
+def create_trigrams(bigrams: List[str], min_count: int = 5) -> List:
     trigram = models.Phrases(bigrams, min_count)
     trigram_mod = models.phrases.Phraser(trigram)
     return [trigram_mod[doc] for doc in bigrams]
 
-def create_tf_ds(X, y=None, shuffle=True):
+
+def create_tf_ds(X: pd.DataFrame, y: Optional[pd.DataFrame] = None,
+                 shuffle: bool = True) -> data.Dataset:
     if y:
         # convert labels into one_hot_encoded labels
         y = utils.to_categorical(y)
@@ -64,7 +82,8 @@ def create_tf_ds(X, y=None, shuffle=True):
             return ds.shuffle((len(ds)))
         return ds
 
-def get_log_dir():
+
+def get_log_dir() -> str:
     root_log_dir = os.path.join(os.curdir, "Transformer Models/my_logs")
     run_id = time.strftime("run_%Y_%m_%d-%H_%M_%S")
     return os.path.join(root_log_dir, run_id)
