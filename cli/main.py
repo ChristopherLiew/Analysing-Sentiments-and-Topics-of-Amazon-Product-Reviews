a51559efa81dbae79a683480f5e5933b6d213015
ChristopherLiew <52255272+ChristopherLiew@users.noreply.github.com>
2021-07-12 09:40:14 +0800

    remove cli dummy code

--- cli/main.py
+++ cli/main.py
@@ -6,21 +6,3 @@ app = typer.Typer()
 # Huggingface Sentiment Clf train + inference (predict)
 app.add_typer(hf_clf.app, name='hf-sent-clf')
 # SKLearn Sentiment Clf
-
-@app.command()
-def load():
-    """
-    Awesome portal gun
-    """
-    typer.echo("Loading rounds")
-    
-
-@app.command()
-def shoot(name: str):
-    """
-    Shoot gun at Name.
-
-    Args:\n
-        name (str): Target of choice
-    """
-    typer.echo(f"Pew {name}!")
