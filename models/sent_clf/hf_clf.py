56f7a4e2d554eb13732491cd1f33eb77cb58281a
ChristopherLiew <52255272+ChristopherLiew@users.noreply.github.com>
2021-07-24 21:01:54 +0800

    Completed inference/predict command for hf classifier and abstracted out util funcs

--- models/sent_clf/hf_clf.py
+++ models/sent_clf/hf_clf.py
@@ -1,45 +1,58 @@
 # TBD
 # 1) Add in artifact logging + metric logging to train and test workflows
 # 2) Figure out how to run remotely on colab
+# 3) Abstract and create util functions for handling Training and Inference Pipeline
 
 import os
+from datasets.arrow_dataset import Dataset
 import typer
 import torch
-import pandas as pd
-import numpy as np
 import wandb
-import logging
-from typing import Callable, Dict, List, Optional
-from datasets import load_dataset, load_metric
+import numpy as np
+from datetime import datetime
+from typing import (
+    List,
+    Optional,
+    Tuple,
+    Union
+)
+from datasets import (
+    load_dataset
+)
 from transformers import (
     AutoTokenizer,
     AutoModelForSequenceClassification,
     DataCollatorWithPadding,
     Trainer,
     TrainingArguments,
-    EvalPrediction,
 )
 from transformers.integrations import (
     TensorBoardCallback
 )
 from pathlib import Path
-
+from ...utils.hf_clf import (
+    get_data_files,
+    compute_clf_metrics,
+    InferenceDataset
+)
 
 # Instatiate Typer App
 app = typer.Typer()
 
 
+WANDB_RUN_NAME = 'amz-hf-sent-clf_' + str(datetime.now())
+
+
 @app.command()
-def train(
-    model_name: str,
-    data_dir: str,
-    num_labels: int = 3,
-    text_col: str = 'text',
-    wandb_entity: str = 'chrisliew',
-    wandb_proj_name: str = 'amz-sent-analysis',
-    wandb_run_name: str = 'amz-hf-sent-clf',
-    wandb_proj_tags: List[str] = ['Test-Run', 'Albert-V2']
-    ) -> None:
+def train(model_name: str,
+          data_dir: str,
+          num_labels: int = 3,
+          text_col: str = 'text',
+          wandb_entity: str = 'chrisliew',
+          wandb_proj_name: str = 'amz-sent-analysis',
+          wandb_run_name: str = WANDB_RUN_NAME,
+          wandb_proj_tags: List[str] = ['Test-Run', 'Albert-V2']
+          ) -> None:
     """
     Train a Huggingface Sequence Classifer.
 
@@ -68,15 +81,15 @@ def train(
     os.environ['WANDB_LOG_MODEL'] = 'true'  # Logs model as an artifact
     os.environ['WANDB_WATCH'] = 'all'  # Logs Gradients and Params
     run = wandb.init(project=wandb_proj_name,
-                    name=wandb_run_name,
-                    tags=wandb_proj_tags,
-                    entity=wandb_entity)
-    
+                     name=wandb_run_name,
+                     tags=wandb_proj_tags,
+                     entity=wandb_entity)
+
     # Constants
     PRE_TRAINED_MODEL_NAME = model_name
     ROOT = Path(data_dir)
     MODEL_SAVE_DIR = Path('logs/hf_clf')
-     
+
     # Check system for GPU
     device = torch.device("cuda") if torch.cuda.is_available()\
         else torch.device("cpu")
@@ -84,41 +97,40 @@ def train(
     typer.secho(f'Device type: {device}', fg=typer.colors.CYAN)
 
     # Dataset filepaths
-    data_files = dict()
-    data_files['train'] = str(ROOT / 'train.csv')
-    data_files['validation'] = str(ROOT / 'validation.csv')
-    data_files['test'] = str(ROOT / 'test.csv')
-    
+    data_files = get_data_files(ROOT)
+
     # Load raw dataset and train-test split
     typer.echo(f'loading train, val and test data from {data_files}')
-    datasets = load_dataset('csv', data_files=data_files)    
-    
+
+    datasets = load_dataset('csv', data_files=data_files)
+
     # Log datasets as Artifacts to W and B
-    typer.echo(f'Logging train, val and test datasets to W and B:')
+    typer.echo('Logging train, val and test datasets to W and B:')
+
     ds_artifact = wandb.Artifact(
-        name='amz_product_reviews_datasets',  # Change to data_dir filename
+        name=wandb_proj_name + '_datasets',
         type='datasets',
-        description="""Processed train, val and test data 
+        description="""Processed train, val and test data
             for hugging face sequence clf models.""",
         metadata={
             "sizes": [v.num_rows for k, v in datasets.items()]
         }
     )
+
     for name, fp in data_files.items():
-        ds_artifact.add_file(fp, name=name)
+        ds_artifact.add_file(fp, name=name + '.csv')
     run.log_artifact(ds_artifact)
-    
+
     # Load Tokenizer and Tokenize
     tokenizer = AutoTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
 
-
     def tokenize_function(examples):
         return tokenizer(examples[text_col],
-                        padding='max_length',
-                        truncation=True)
+                         padding='max_length',
+                         truncation=True)
 
-    typer.echo(f'Tokenizing data:')
-    
+    typer.echo('Tokenizing data:')
+
     tokenized_datasets = datasets.map(
         tokenize_function,
         batched=True
@@ -133,39 +145,12 @@ def train(
         num_labels=num_labels
     )
 
-    # Load Metrics
-    acc_metric = load_metric('accuracy')
-    f1_metric = load_metric('f1')
-    prec_metric = load_metric('precision')
-    rec_metric = load_metric("recall")
-
-
-    def compute_metrics(eval_pred: EvalPrediction
-                        ) -> Callable[[EvalPrediction], Dict]:
-        logits, labels = eval_pred
-        predictions = np.argmax(logits, axis=-1)
-        metrics = {
-            'accuracy': acc_metric.compute(predictions=predictions,
-                                           references=labels),
-            'f1': f1_metric.compute(predictions=predictions,
-                                    references=labels,
-                                    average='macro'),
-            'precision': prec_metric.compute(predictions=predictions,
-                                            references=labels,
-                                            average='macro'),
-            'recall': rec_metric.compute(predictions=predictions,
-                                        references=labels,
-                                        average='macro')
-        }
-        return metrics
-
-
-    # Training
+    # Training (Change to **kwargs)
     training_args = TrainingArguments(
         report_to='wandb',
         output_dir=MODEL_SAVE_DIR,
         load_best_model_at_end=True,
-        evaluation_strategy='steps',
+        evaluation_strategy='epoch',
         per_device_train_batch_size=32,
         per_device_eval_batch_size=32,
         num_train_epochs=5,
@@ -187,34 +172,34 @@ def train(
         train_dataset=tokenized_datasets['train'],
         eval_dataset=tokenized_datasets['validation'],
         data_collator=data_collator,
-        compute_metrics=compute_metrics,
+        compute_metrics=compute_clf_metrics,
         callbacks=[tb_cb]
     )
 
     # Train model
-    typer.echo(f'Starting up model training ...')
+    typer.secho('Starting up model training ...', fg=typer.colors.YELLOW)
     # trainer.train()
 
     # Evaluate model
-    typer.echo(f'Starting up model evaluation ...')
+    typer.secho('Starting up model evaluation ...', fg=typer.colors.YELLOW)
     trainer.evaluate()
 
     # End W and B session
     wandb.finish()
-    
-    typer.secho('Training and Evaluation Completed', fg=typer.colors.GREEN)
 
+    typer.secho('Training and Evaluation Completed', fg=typer.colors.GREEN)
 
 
 # Model Inference (TB tested and refined)
 @app.command()
 def predict(model_name: Optional[str],  # Check if we can infer from model artifact
-            wandb_proj_name: str,
             wandb_run_name: str,
-            num_labels: int,  # Check if we can infer this from the configs
+            wandb_entity: str = 'chrisliew',
+            wandb_proj_name: str = 'amz-sent-analysis',
+            num_labels: int = 3,  # Check if we can infer this from the configs
             inf_data: Optional[str] = None,
             text_col: Optional[str] = 'text'
-            ):
+            ) -> Tuple[List[Union[int, float]], List[Union[int, float]]]:
     """
     Perform inference using the latest huggingface classifier trained on a given
     set of test data.
@@ -227,39 +212,51 @@ def predict(model_name: Optional[str],  # Check if we can infer from model artif
         wandb_run_name (str): W and B run name within the project.\n
         num_labels (int): Number of labels.\n
         text_col (Optional[str]): Name of text column. Defaults to 'text'.\n
-    
+
     Returns:\n
         List[...]: Huggingface sequence classifier inference output.
     """
 
     with wandb.init(project=wandb_proj_name) as run:
-        
-        # Load latest trained model
+
+        # Load latest trained model (To be tested)
         my_model_name = f"{wandb_run_name}:latest"
         my_model_artifact = run.use_artifact(my_model_name)
         model_dir = my_model_artifact.download()
-        
+
         model = AutoModelForSequenceClassification.from_pretrained(
-            model_dir, 
-            num_labels=num_labels)
-        
-        # Load and process test data (Pull latest test data else load and process)
+            model_dir,
+            num_labels=num_labels
+        )
+
+        # Load and process test data
         if inf_data is not None:
-            assert model_name is not None, 'Please provide the corresponding model name'
             test_data = load_dataset('csv', data_files=inf_data)
-            
-            # Preprocess data
-            tokenizer = AutoTokenizer(model_name)
-            tokenized_test_data = tokenizer(
-                test_data[text_col],
-                truncation=True,
-                padding='max_length'
-            )
-            output = model(**tokenized_test_data)
-        
         else:
-            # TBD
-            # test_ds = run.use_artifact('amz_product_reviews_datasets/test')
-            pass
-        # Process results
-        return output
+            # Pull latest test dataset from W and B
+            ds_wnb_fp = os.path.join(wandb_entity,
+                                     wandb_proj_name,
+                                     wandb_proj_name,
+                                     '_datasets:latest')
+            artifacts = run.use_artifact(ds_wnb_fp, type='datasets')
+            artifacts_dir = Path(artifacts.download())
+            data_files = get_data_files(artifacts_dir)
+            # Format artifacts_dir into train, val and test dict
+            test_data = load_dataset('csv', data_files=data_files)['test']
+
+        # Preprocess data
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        tokenized_test_data = tokenizer.batch_encode_plus(
+            test_data[text_col],
+            truncation=True,
+            padding='max_length'
+        )
+        
+        # Make Inference
+        test_dataset = InferenceDataset(tokenized_test_data)
+        test_trainer = Trainer(model)
+        raw_pred, _, _ = test_trainer.predict(test_dataset)
+        y_pred = np.argmax(raw_pred, axis=1)
+        y_true = test_data['label']
+
+        return y_true, y_pred
---
+++ utils/hf_clf.py
@@ -0,0 +1,81 @@
+"""
+Huggingface Sequence Classifier Helper / Utility Functions.
+"""
+import numpy as np
+from typing import (
+    Callable,
+    Dict
+)
+from datasets import (
+    load_metric
+)
+from transformers import (
+    EvalPrediction
+)
+import torch
+from torch.utils.data import Dataset
+from pathlib import Path
+
+
+# Load Metrics
+acc_metric = load_metric('accuracy')
+f1_metric = load_metric('f1')
+prec_metric = load_metric('precision')
+rec_metric = load_metric("recall")
+
+
+def compute_clf_metrics(eval_pred: EvalPrediction
+                        ) -> Callable[[EvalPrediction], Dict]:
+
+    logits, labels = eval_pred
+
+    predictions = np.argmax(logits, axis=-1)
+
+    metrics = {
+        'accuracy': acc_metric.compute(predictions=predictions,
+                                       references=labels),
+
+        'f1': f1_metric.compute(predictions=predictions,
+                                references=labels,
+                                average='macro'),
+
+        'precision': prec_metric.compute(predictions=predictions,
+                                         references=labels,
+                                         average='macro'),
+
+        'recall': rec_metric.compute(predictions=predictions,
+                                     references=labels,
+                                     average='macro')
+    }
+    return metrics
+
+
+def get_data_files(data_dir_root: Path,
+                   format: str = 'csv'
+                   ) -> Dict[str, str]:
+    data_files = dict()
+    train_path, val_path, test_path = (
+        f'train.{format}',
+        f'validation.{format}',
+        f'test.{format}'
+    )
+    data_files['train'] = str(data_dir_root / train_path)
+    data_files['validation'] = str(data_dir_root / val_path)
+    data_files['test'] = str(data_dir_root / test_path)
+    return data_files
+
+
+class InferenceDataset(Dataset):
+    def __init__(self, encodings, labels=None):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val
+                in self.encodings.items()}
+        if self.labels:
+            item["labels"] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.encodings["input_ids"])
